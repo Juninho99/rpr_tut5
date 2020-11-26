31c6b86a02ae46f39014dc9f122c13dc81c47bda
Armin Žunić <azunic1@etf.unsa.ba>
2020-11-26 23:42:45 +0100

    Dodani konstruktori i geteri i seteri za sve klase

--- src/ba/unsa/etf/rpr/Predmet.java
+++ src/ba/unsa/etf/rpr/Predmet.java
@@ -1,9 +1,40 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Predmet {
     private String ime;
-    private int etcs;
+    private int ects;
     private List<Student> studenti;
+
+    public Predmet(String ime, int ects) {
+        this.ime = ime;
+        this.ects = ects;
+        studenti = new ArrayList<>();
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public int getEcts() {
+        return ects;
+    }
+
+    public void setEcts(int ects) {
+        this.ects = ects;
+    }
+
+    public List<Student> getStudenti() {
+        return studenti;
+    }
+
+    public void setStudenti(List<Student> studenti) {
+        this.studenti = studenti;
+    }
 }
--- src/ba/unsa/etf/rpr/Semestar.java
+++ src/ba/unsa/etf/rpr/Semestar.java
@@ -1,9 +1,40 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Semestar {
     private String ime;
     private List<Predmet> obavezniPredmeti;
     private List<Predmet> izborniPredmeti;
+
+    public Semestar(String ime) {
+        this.ime = ime;
+        obavezniPredmeti = new ArrayList<>();
+        izborniPredmeti = new ArrayList<>();
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public List<Predmet> getObavezniPredmeti() {
+        return obavezniPredmeti;
+    }
+
+    public void setObavezniPredmeti(List<Predmet> obavezniPredmeti) {
+        this.obavezniPredmeti = obavezniPredmeti;
+    }
+
+    public List<Predmet> getIzborniPredmeti() {
+        return izborniPredmeti;
+    }
+
+    public void setIzborniPredmeti(List<Predmet> izborniPredmeti) {
+        this.izborniPredmeti = izborniPredmeti;
+    }
 }
